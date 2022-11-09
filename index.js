a77d81016b4f6fdcfa0b25c9d796a200f3b25a57
Denis Barbosa <denis_barbosa123@hotmail.com>
2022-11-08 21:24:09 -0300

    fix delete

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const ul = document.querySelector('ul')
 
 var itensDB = []
 
-db.version(1).stores({
-    todo: "++id,list"
-})
+db.version(1).stores({todo: "_id"})
 
 db.on("populate", async () => {
     await db.todo.bulkPut([
       {
+        _id: String(Date.now()),
         item: "Learn English",
-        status: "checked"
+        status: ""
       },
       {
+        _id: String(Date.now()),
         item: "Learn JS",
         status: ""
       },
@@ -31,17 +31,15 @@ db.open()
 // Click handler for entire DIV container
 ul.addEventListener('click', async function (e) {
   if(e.target.classList.contains('bx-trash')){
-    await removeItem(e.target.getAttribute("data-i"))
+    removeItem(e.target.getAttribute("id"))
   }
   if(e.target.classList.contains('checkbox-edit')){
-    await done(e.target.checked, e.target.getAttribute("data-i"))
+    done(e.target.checked, e.target.getAttribute("id"))
   }
 });
 
-
-btnDeleteAll.onclick = async () => {
-  await db.todo.clear()
-  await loadItens()
+btnDeleteAll.onclick = () => {
+   db.todo.clear().then(loadItens)
 }
 
 texto.addEventListener('keypress', e => {
@@ -57,59 +55,56 @@ btnInsert.onclick = () => {
 }
 
 async function setItemDB() {
-  await db.todo.add({
+  db.todo.add({
+    _id: String(Date.now()),
     item: texto.value,
     status: ''
-  })
-  await loadItens()
+  }).then(loadItens)
 }
 
 async function loadItens() {
   ul.innerHTML = "";
   itensDB = await db.todo.toArray()
-  itensDB.forEach((item, i) => {
-    insertItemTela(item.item, item.status, i)
+  itensDB.forEach((item) => {
+    insertItemTela(item.item, item.status, item._id)
   })
 }
 
-function insertItemTela(text, status, i) {
+function insertItemTela(text, status, id) {
   const li = document.createElement('li')
   
   li.innerHTML = `
     <div class="divLi">
-      <input class="checkbox-edit "type="checkbox" ${status} data-i=${i+1} />
-      <span data-si=${i+1}>${text}</span>
-      <button data-i=${i+1}><i data-i=${i+1} class='bx bx-trash'></i></button>
+      <input class="checkbox-edit "type="checkbox" ${status} id=${id} />
+      <span id=${id}>${text}</span>
+      <button id=${id}><i id=${id} class='bx bx-trash'></i></button>
     </div>
     `
   ul.appendChild(li)
 
   if (status) {
-    document.querySelector(`[data-si="${i+1}"]`).classList.add('line-through')
+    document.querySelector(`[id="${id}"]`).classList.add('line-through')
   } else {
-    document.querySelector(`[data-si="${i+1}"]`).classList.remove('line-through')
+    document.querySelector(`[id="${id}"]`).classList.remove('line-through')
   }
 
   texto.value = ''
 }
 
-async function done(chk, i) {
+async function done(chk, id) {
   itensDB = await db.todo.toArray()
-  let item = itensDB.filter(item => item.id == i)[0]
+  let item = itensDB.filter(item => item._id == id)[0]
   if(chk){
     item.status = 'checked'
   }else{
     item.status = ''
   }
   
-  await db.todo.put(item,i);
-  await loadItens()
+  db.todo.put(item,id).then(loadItens)
 }
 
-async function removeItem(i) {
-  console.log(i)
-  await db.todo.delete(i)
-  await loadItens()
+function removeItem(id) {
+  db.todo.where('_id').equals(id).delete().then(loadItens)
 }
 
 await loadItens()
